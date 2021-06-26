23d45ee53cc5ffd2613b5773a94f5c1151caab18
shaedrizwan <50213576+shaedrizwan@users.noreply.github.com>
2021-06-26 15:08:47 +0530

    Feat: Authentication implimented

--- src/authContext.js
+++ src/authContext.js
@@ -1,15 +1,51 @@
-import {createContext,useContext} from "react";
+import axios from "axios";
+import {createContext,useContext, useEffect, useState} from "react";
+import { useLocation,useNavigate } from "react-router-dom";
 
 const authContext = createContext();
+
+
 
 export function useAuth(){
     return useContext(authContext)
 }
 
 export function AuthProvider({children}){
-    const namer = () => console.log("Auth Context working")
+
+    useEffect(()=>{
+        const loginStatus = JSON.parse(localStorage?.getItem("login"));
+        loginStatus?.isLogged && setLogin(true);
+    },[])
+
+
+    const {state} = useLocation();
+    const navigate = useNavigate();
+
+    const [login,setLogin] = useState(false)
+
+    const checkAuth = async (username,password) =>{
+        if(!login){
+            console.log(username,password)
+            try{
+                const response = await axios.post("https://cryptotube-backend.herokuapp.com/user/login",{username,password})
+                if(response.status === 200){
+                    setLogin(true)
+                    localStorage?.setItem("login",JSON.stringify({isLogged:true}))
+                    if(state!= null) navigate(state.from)
+                }
+            }
+            catch{
+                console.log("Invalid Username/Password")
+            }
+        }
+        else{
+            setLogin(false)
+            localStorage?.removeItem("login")
+        }
+    }
+
     return (
-    <authContext.Provider value={{namer}}>
+    <authContext.Provider value={{login,checkAuth}}>
         {children}
     </authContext.Provider>
     )
