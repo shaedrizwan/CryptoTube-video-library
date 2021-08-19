9699b2e74a8b19daace9e1f8b7f04fbe65710a9a
shaedrizwan <50213576+shaedrizwan@users.noreply.github.com>
2021-08-19 16:04:47 +0530

    Style: Video card responsiveness added

--- src/components/VideoList/VideoList.jsx
+++ src/components/VideoList/VideoList.jsx
@@ -6,10 +6,10 @@ function VideoList({video,dispatchFunction}) {
     return (
         <div className="video-container" key={video._id}>
             <Link className="video-link-wrapper" to={`/video/${video.slug}`}>
-                <img className="video-img" style={{maxWidth:"350px",height:"fit-content"}} src={video.thumbnail} alt={video.slug}/>
+                <img className="video-thumbnail" src={video.thumbnail} alt={video.slug}/>
                 <div className="video-title">{video.title}</div>
                 <div className="video-details-container">
-                    <img src="https://yt3.ggpht.com/ytc/AKedOLRKlMd8XiIOUXc9DKEjUgt5fFy1OJgYkN9tF90F4w=s68-c-k-c0x00ffffff-no-rj" alt="profile" style={{width:"40px", height:"fit-content", borderRadius:"50%",marginRight:"10px"}}/>
+                    <img src="https://yt3.ggpht.com/ytc/AKedOLRKlMd8XiIOUXc9DKEjUgt5fFy1OJgYkN9tF90F4w=s68-c-k-c0x00ffffff-no-rj" className="video-channel-thumbnail" alt="profile" />
                     <div>
                         <div className="video-channel">{video.channel_name}</div>
                         <div className="video-date">{video.published_date}</div>
--- src/pages/VideoDetails.js
+++ src/pages/VideoDetails.js
@@ -107,7 +107,7 @@ export function VideoDetails(){
             </div>
 
             <div className="details-container">
-                <img src="https://yt3.ggpht.com/ytc/AKedOLRKlMd8XiIOUXc9DKEjUgt5fFy1OJgYkN9tF90F4w=s68-c-k-c0x00ffffff-no-rj" alt="profile" style={{width:"40px", height:"fit-content", borderRadius:"50%",marginRight:"10px"}}/>
+                <img src="https://yt3.ggpht.com/ytc/AKedOLRKlMd8XiIOUXc9DKEjUgt5fFy1OJgYkN9tF90F4w=s68-c-k-c0x00ffffff-no-rj" alt="profile" className="channel-thumbnail" />
                 <div className="details-container-right">
                     <div className="details-channel">{video.channel_name}</div>
                     <div className="details-date">{video.published_date}</div>
